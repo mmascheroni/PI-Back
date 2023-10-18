e512d707b4d2ac7dd6cb7515366ed43f60d9534d
Serrana Marset <serranamarset@gmail.com>
2023-10-18 20:15:11 -0300

    Inicializamos proyecto

---
+++ src/main/java/com/backend/PIBack/PiBackApplication.java
@@ -0,0 +1,13 @@
+package com.backend.PIBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiBackApplication.class, args);
+	}
+
+}
