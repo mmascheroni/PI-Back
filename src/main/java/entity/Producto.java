fe66645f0c6d6a3ec56a2bfb291273baf9260bec
Serrana Marset <serranamarset@gmail.com>
2023-10-18 20:47:47 -0300

    Se crea package entity y entidad producto

---
+++ src/main/java/entity/Producto.java
@@ -0,0 +1,45 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "PRODUCTOS")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+   //validación nombre único
+    private String nombre;
+
+    private String descripcion;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
