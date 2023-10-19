382040922b618e2da88530efe1b36c5af32c4eca
Serrana Marset <serranamarset@gmail.com>
2023-10-18 22:04:57 -0300

    Se inicializa el archivo main.

--- src/main/java/com/backend/PIBack/PiBackApplication.java
+++ src/main/java/com/backend/PIBack/PiBackApplication.java
@@ -1,13 +1,18 @@
 package com.backend.PIBack;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PiBackApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(PiBackApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(PiBackApplication.class, args);
+		LOGGER.info("Inicializando Sinfonía Perfecta ...");
 	}
 
 }
