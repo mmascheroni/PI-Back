cbd640c8b289d54531ca267a3c0e8cee79266a29
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-10-21 20:30:34 -0300

    Modificando entidad Imagen e ImagenDto

--- src/main/java/com/backend/PIBack/dto/ImagenDto.java
+++ src/main/java/com/backend/PIBack/dto/ImagenDto.java
@@ -1,2 +1,49 @@
-package com.backend.PIBack.dto;public class ImagenDto {
+package com.backend.PIBack.dto;
+
+import com.backend.PIBack.entity.Imagen;
+import com.backend.PIBack.entity.Producto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ImagenDto {
+
+    private Long id;
+
+    private String url;
+
+    private Producto producto;
+
+    public ImagenDto() {
+    }
+
+    public ImagenDto(Long id, String url, Producto producto) {
+        this.id = id;
+        this.url = url;
+        this.producto = producto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
 }
--- src/main/java/com/backend/PIBack/entity/Imagen.java
+++ src/main/java/com/backend/PIBack/entity/Imagen.java
@@ -1,4 +1,48 @@
 package com.backend.PIBack.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "IMAGENES")
 public class Imagen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    public Imagen() {}
+
+    public Imagen(String url, Producto producto) {
+        this.url = url;
+        this.producto = producto;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
 }
