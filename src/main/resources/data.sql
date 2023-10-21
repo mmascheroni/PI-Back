02a671cab8f46d2cf5f7b5b979678570c9c3ad0f
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-10-21 20:36:45 -0300

    Modificando Base de Datos a MYSQL y se agrega script con insercion de datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,78 @@
-INSERT INTO productos (nombre, descripcion) VALUES ("Guitarra Clasica", "Guitarra Clasica")
+INSERT INTO productos (nombre, descripcion) VALUES ("Guitarra Clasica Stagg C405 1/4 natural", "Guitarra clásica tamaño 1/4, recomendada especialmente para niños de (aprox.) entre 4 a 6 años.");
 
-INSERT INTO imagenes (url, product_id) VALUES ("https://f.fcdn.app/imgs/7a2e8d/www.coutinhomusic.com/coutuy/ca66/webp/catalogo/RG370AHMZ-RG370AHMZ_1/2000-2000/guitarra-electrica-ibanez-rg370-bmt-guitarra-electrica-ibanez-rg370-bmt.jpg"", 1)
+INSERT INTO productos (nombre, descripcion) VALUES ("Guitarra clasica Stagg SCL50 black", "Guitarra clásica 4/4 construida con cuerpo de linden (tilo norteamericano), mástil de nato y diapasón y puente de pau ferro. ");
+
+INSERT INTO productos (nombre, descripcion) VALUES ("Guitarra Clásica APC GC200 OP Lusitana", "Esta guitarra clásica modelo Lusitana posee tapa de pino oregón, laterales y fondo de sapelli y terminación mate.");
+
+INSERT INTO productos (nombre, descripcion) VALUES ("GUITARRA ELECTROACÚSTICA NEGRO", "Cuerpo Dreadnough Superior: Tapa de abeto Aros y fondo: Caoba atras y los lados Ropacka: Blanco y negro Multi ropacka");
+
+INSERT INTO productos (nombre, descripcion) VALUES ("Pedal de efecto Reverb hall of fame 2", "Reverb La nueva tecnología TC MASH combina el pulsador de pie con un controlador de expresión sensible a la presión Nuevo algoritmo Shimmer Reverb El Analog-Dry Through mantiene la integridad de la señal seca cuando la Reverb se encuentra conectada");
+
+INSERT INTO productos (nombre, descripcion) VALUES ("GUITARRA ELECTRICA RG350DXZ BLANCO", "Este elegante Ibanez cuenta con el sistema de trémolo Edge Zero II para un trabajo de golpe impecable y preciso.");
+
+INSERT INTO productos (nombre, descripcion) VALUES ("Guitarra Eléctrica Rg370 Bmt", "Sus pastillas Quantum proporcionan una respuesta de graves acelerada Mástil de arce Wizard III, especialmente diseñado para estilos rápidos Su puente Edge-Zero II permite una afinación rápida, sencilla y estable");
+
+INSERT INTO productos (nombre, descripcion) VALUES ("Bajo eléctrico SR300E IPT", "Durante 25 años la serie SR ha dado a los bajistas una alternativa moderna. Con su popularidad mantenida a lo largo de los años, Ibanez está constantemente procurando dar respuesta a las más amplias necesidades de gran variedad de bajistas y presupuestos.");
+
+INSERT INTO productos (nombre, descripcion) VALUES ("Bajo eléctrico TMB100 TALMAN BK", "BAJO ACTIVO IBANEZ TALMAN TMB100 BK NEGRO Podes esperar un tono poderoso y con golpe de este cuerpo de álamo y mango de maple.");
+
+INSERT INTO productos (nombre, descripcion) VALUES ("PEDAL EFECTOS TRV1 REVERB SHIM VERB PRO", "El Mooer TVR1 es un auténtico motor y uno de los pedales de reverberación más complejos del mercado.");
+
+--Imagen Producto 1
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/0e16f3/www.coutinhomusic.com/coutuy/12e8/webp/catalogo/C405MNAT-C405MNAT_1/2000-2000/guitarra-clasica-stagg-c405-1-4-natural-guitarra-clasica-stagg-c405-1-4-natural.jpg", 1);
+
+--Imagen Producto 2
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/3cc179/www.coutinhomusic.com/coutuy/640b/webp/catalogo/SCL50BLK-SCL50BLK_1/2000-2000/guitarra-clasica-stagg-scl50-black-guitarra-clasica-stagg-scl50-black.jpg", 2);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/4a1da7/www.coutinhomusic.com/coutuy/5aa7/webp/catalogo/SCL50BLK-SCL50BLK_4/2000-2000/guitarra-clasica-stagg-scl50-black-guitarra-clasica-stagg-scl50-black.jpg", 2);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/79b571/www.coutinhomusic.com/coutuy/07e2/webp/catalogo/SCL50BLK-SCL50BLK_2/2000-2000/guitarra-clasica-stagg-scl50-black-guitarra-clasica-stagg-scl50-black.jpg", 2);
+
+--Imagen Producto 3
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/9dde57/www.coutinhomusic.com/coutuy/c872/webp/catalogo/GC200OP-GC200OP_1/2000-2000/guitarra-clasica-apc-gc200-op-lusitana-guitarra-clasica-apc-gc200-op-lusitana.jpg", 3);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/dfbd9d/www.coutinhomusic.com/coutuy/274b/webp/catalogo/GC200OP-GC200OP_2/2000-2000/guitarra-clasica-apc-gc200-op-lusitana-guitarra-clasica-apc-gc200-op-lusitana.jpg", 3);
+
+--Imagen Producto 4
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/bf054e/www.coutinhomusic.com/coutuy/51d5/webp/catalogo/PF15ECEBK-PF15ECEBK_1/2000-2000/guitarra-electroacustica-ibanez-pf15ece-negro-guitarra-electroacustica-ibanez-pf15ece-negro.jpg", 4);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/bfb2d8/www.coutinhomusic.com/coutuy/cc2a/webp/catalogo/PF15ECEBK-PF15ECEBK_2/2000-2000/guitarra-electroacustica-ibanez-pf15ece-negro-guitarra-electroacustica-ibanez-pf15ece-negro.jpg", 4);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/22ea30/www.coutinhomusic.com/coutuy/000f/webp/catalogo/PF15ECEBK-PF15ECEBK_3/2000-2000/guitarra-electroacustica-ibanez-pf15ece-negro-guitarra-electroacustica-ibanez-pf15ece-negro.jpg", 4);
+
+--Imagen Producto 5
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/9fc7b4/www.coutinhomusic.com/coutuy/f002/webp/catalogo/CI300-CI300_1/2000-2000/pedal-de-efecto-reverb-hall-of-fame-2-tc-electronic-pedal-de-efecto-reverb-hall-of-fame-2-tc-electronic.jpg", 5);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/bb2276/www.coutinhomusic.com/coutuy/3c3e/webp/catalogo/CI300-CI300_2/2000-2000/pedal-de-efecto-reverb-hall-of-fame-2-tc-electronic-pedal-de-efecto-reverb-hall-of-fame-2-tc-electronic.jpg", 5);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/e065e5/www.coutinhomusic.com/coutuy/bc59/webp/catalogo/CI300-CI300_3/2000-2000/pedal-de-efecto-reverb-hall-of-fame-2-tc-electronic-pedal-de-efecto-reverb-hall-of-fame-2-tc-electronic.jpg", 5);
+
+--Imagen Producto 6
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/5416f7/www.coutinhomusic.com/coutuy/5f8a/webp/catalogo/RG350DXZWH-RG350DXZWH_1/2000-2000/guitarra-electrica-ibanez-rg350dxz-blanco-guitarra-electrica-ibanez-rg350dxz-blanco.jpg", 6);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/ca6d5c/www.coutinhomusic.com/coutuy/70fa/webp/catalogo/RG350DXZWH-RG350DXZWH_2/2000-2000/guitarra-electrica-ibanez-rg350dxz-blanco-guitarra-electrica-ibanez-rg350dxz-blanco.jpg", 6);
+
+--Imagen Producto 7
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/7a2e8d/www.coutinhomusic.com/coutuy/ca66/webp/catalogo/RG370AHMZ-RG370AHMZ_1/2000-2000/guitarra-electrica-ibanez-rg370-bmt-guitarra-electrica-ibanez-rg370-bmt.jpg", 7);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/c78611/www.coutinhomusic.com/coutuy/b296/webp/catalogo/RG370AHMZ-RG370AHMZ_2/2000-2000/guitarra-electrica-ibanez-rg370-bmt-guitarra-electrica-ibanez-rg370-bmt.jpg", 7);
+
+--Imagen Producto 8
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/36753a/www.coutinhomusic.com/coutuy/4fde/webp/catalogo/SR300EIPT-SR300EIPT_1/2000-2000/bajo-electrico-ibanez-sr300e-ipt-bajo-electrico-ibanez-sr300e-ipt.jpg", 8);
+
+--Imagen Producto 9
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/6b447a/www.coutinhomusic.com/coutuy/d1be/webp/catalogo/TMB100BK-TMB100BK_1/2000-2000/bajo-electrico-ibanez-tmb100-talman-bk-bajo-electrico-ibanez-tmb100-talman-bk.jpg", 9);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/a82c60/www.coutinhomusic.com/coutuy/6cf0/webp/catalogo/TMB100BK-TMB100BK_2/2000-2000/bajo-electrico-ibanez-tmb100-talman-bk-bajo-electrico-ibanez-tmb100-talman-bk.jpg", 9);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/ffa82f/www.coutinhomusic.com/coutuy/9e2d/webp/catalogo/TMB100BK-TMB100BK_3/2000-2000/bajo-electrico-ibanez-tmb100-talman-bk-bajo-electrico-ibanez-tmb100-talman-bk.jpg", 9);
+
+
+--Imagen Producto 10
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/69aa95/www.coutinhomusic.com/coutuy/c88b/webp/catalogo/TRV1-TRV1_1/2000-2000/pedal-efectos-mooer-trv1-reverb-shim-verb-pro-pedal-efectos-mooer-trv1-reverb-shim-verb-pro.jpg", 10);
+
+INSERT INTO imagenes (url, producto_id) VALUES ("https://f.fcdn.app/imgs/d45dc6/www.coutinhomusic.com/coutuy/fc34/webp/catalogo/TRV1-TRV1_2/2000-2000/pedal-efectos-mooer-trv1-reverb-shim-verb-pro-pedal-efectos-mooer-trv1-reverb-shim-verb-pro.jpg", 10);
+
+
+
 
