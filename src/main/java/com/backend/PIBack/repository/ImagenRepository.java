aeb10dd6dbeb82ec9aa37c2ea71d75b39e2a4492
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-10-21 20:35:13 -0300

    Modificando ImagenRepository

--- src/main/java/com/backend/PIBack/repository/ImagenRepository.java
+++ src/main/java/com/backend/PIBack/repository/ImagenRepository.java
@@ -1,2 +1,7 @@
-package com.backend.PIBack.repository;public interface ImagenRepository {
+package com.backend.PIBack.repository;
+
+import com.backend.PIBack.entity.Imagen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImagenRepository extends JpaRepository<Imagen, Long> {
 }
