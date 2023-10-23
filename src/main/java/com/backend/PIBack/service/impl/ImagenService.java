51112ec84be1c4cad10d2dd8cf7e106dd045f0b2
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-10-22 21:18:48 -0300

    Modificando application.properties

--- src/main/java/com/backend/PIBack/service/impl/ImagenService.java
+++ src/main/java/com/backend/PIBack/service/impl/ImagenService.java
@@ -1,11 +1,7 @@
 package com.backend.PIBack.service.impl;
 
-//import com.backend.PIBack.dto.ImagenDto;
-//import com.backend.PIBack.entity.Imagen;
 import com.backend.PIBack.dto.ImagenDto;
-import com.backend.PIBack.dto.ProductoDto;
 import com.backend.PIBack.entity.Imagen;
-import com.backend.PIBack.entity.Producto;
 import com.backend.PIBack.repository.ImagenRepository;
 import com.backend.PIBack.service.IImagenService;
 import com.fasterxml.jackson.databind.ObjectMapper;
